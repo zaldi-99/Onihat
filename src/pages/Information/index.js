ca995e53ff5227328d40e30608b10fb59ef20b24
zaldi-99 <rizkyzaldi@gmail.com>
2021-03-30 17:33:12 +0700

    post data and info page

--- src/pages/Information/index.js
+++ src/pages/Information/index.js
@@ -1,22 +1,105 @@
-import React from 'react'
-import { StatusBar, StyleSheet, Text, View } from 'react-native'
-import Header from '../../components/Header'
-import { colors } from '../../utils'
+import React from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import Header from "../../components/Header";
+import { colors } from "../../utils";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
-const Information = () => {
-    return (
-        <View style={styles.container}>
-            <StatusBar barStyle="default" />
-            <Text>Information about Onihat</Text>
+const Information = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="default" />
+      <TouchableOpacity
+        style={{ marginTop: 20 }}
+        onPress={() => navigation.goBack()}
+      >
+        <Image
+          source={require("../../assets/Logo-Onihat-Big.png")}
+          style={styles.imageWrapper}
+        />
+      </TouchableOpacity>
+      <View>
+        <View>
+          <Text style={styles.desc}>
+            This is a application for users who want to pour out all their
+            positive and negative thoughts, everything that has been told is
+            anonymous so there is no need to worry that your personal data will
+            not be known by anyone
+          </Text>
+          <View
+            style={{
+              height: 10,
+              borderTopWidth: 3,
+              width: 100,
+              alignSelf: "center",
+              marginTop: 15,
+              borderTopColor: "purple",
+            }}
+          ></View>
+          <Text
+            style={{
+              alignSelf: "center",
+              textAlign: "center",
+              fontSize: 20,
+              color: "purple",
+              marginTop: 25,
+            }}
+          >
+            IF YOU HAVE ANY SUGGESTION {"\n"}
+            CONTACT US ON EMAIL{" "}
+          </Text>
+          <TouchableOpacity style={styles.button}>
+            <Icon name="email" size={20} />
+          </TouchableOpacity>
         </View>
-    )
-}
+        <View style={{ marginTop: 200 }}>
+          <Text
+            style={{ textAlign: "center", color: "purple", fontWeight: "bold" }}
+          >
+            THIS APPLICATION IS PRODUCED AND PUBLISHED BY {"\n"}
+            Wanna Be Start
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
 
-export default Information
+export default Information;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#181A27'
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#181A27",
+  },
+  imageWrapper: {
+    width: 260,
+    height: 75,
+    alignSelf: "center",
+    marginTop: 20,
+  },
+  desc: {
+    width: "75%",
+    alignSelf: "center",
+    textAlign: "center",
+    fontSize: 18,
+    color: "purple",
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: "purple",
+    paddingHorizontal: 12,
+    paddingVertical: 11,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+    marginHorizontal: 35,
+  },
+});
--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,22 +1,157 @@
-import React from 'react'
-import { StatusBar, StyleSheet, Text, View } from 'react-native'
-import Header from '../../components/Header'
+import React from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import Header from "../../components/Header";
 
-const Post = () => {
+// const Post = () => {
+//   return (
+//     <View style={styles.container}>
+//       <StatusBar barStyle="default" />
+//       <Header />
+//       <Text>Post Screen</Text>
+//     </View>
+//   );
+// };
+
+// const Post = ({ navigation }) => {
+//   const request = new Request(
+//     "https://api-mading.wannabestart.com/public/api/mading",
+//     {
+//       method: "POST",
+//       headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         firstParam: "dear",
+//         secondParam: "negatif_post",
+//       }),
+//     }
+//   );
+//   return (
+//     <View style={styles.container}>
+//       <StatusBar barStyle="default" />
+//       <Header onPress={() => navigation.navigate("Information")} />
+//       <Text style={{ color: "white" }}>Post Screen</Text>
+//     </View>
+//   );
+// };
+
+// export default Post;
+
+export default class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      dear: "",
+      negatif_post: "",
+      rapinam: "hate tokyo",
+    };
+  }
+
+  async onFetchMading() {
+    const data = {
+      dear: this.state.dear,
+      negatif_post: this.state.negatif_post,
+      rapinam: this.state.rapinam,
+    };
+    try {
+      let response = await fetch(
+        "https://api-mading.wannabestart.com/public/api/mading",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      if (response.status >= 201) {
+        alert("Succesfull Post Data");
+      }
+    } catch (errors) {
+      alert(errors);
+    }
+  }
+
+  onPressSubmitButton() {
+    this.onFetchMading();
+    // alert('Berhasil Submit');
+  }
+
+  render() {
     return (
-        <View style={styles.container}>
-            <StatusBar barStyle="default" />
-            <Header />
-            <Text>Post Screen</Text>
+      <View style={styles.container}>
+        <StatusBar barStyle="default" />
+        <Header />
+
+        {/* Content */}
+        <View style={styles.form}>
+          <TextInput
+            autoFocus={true}
+            onChangeText={(text) => this.setState({ dear: text })}
+            placeholder="Dear, "
+            style={styles.styleDear}
+          ></TextInput>
+          <TextInput
+            onChangeText={(text) => this.setState({ negatif_post: text })}
+            placeholder="What's on your mind"
+            multiline={true}
+            style={styles.stylePesan}
+          ></TextInput>
+          <TouchableOpacity
+            onPress={this.onPressSubmitButton.bind(this)}
+            style={styles.button}
+          >
+            <Text style={{ color: "white" }}>Submit</Text>
+          </TouchableOpacity>
         </View>
-    )
+      </View>
+    );
+  }
 }
 
-export default Post
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#181A27'
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#181A27",
+  },
+  form: {
+    marginHorizontal: 18,
+    backgroundColor: "#24263B",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginTop: 15,
+  },
+  styleDear: {
+    paddingHorizontal: 5,
+    borderBottomWidth: 1,
+    margin: 10,
+    paddingBottom: 5,
+    color: "white",
+  },
+  stylePesan: {
+    paddingHorizontal: 5,
+    paddingBottom: 5,
+    marginHorizontal: 10,
+    marginBottom: 5,
+    color: "white",
+  },
+  button: {
+    backgroundColor: "#1B3BBB",
+    paddingHorizontal: 12,
+    paddingVertical: 11,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50,
+  },
+});
